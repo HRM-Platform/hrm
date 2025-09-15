ba4afa990a48ab76db7e6052ffe1873c1f86dc09
Engr Altaf <engraltaf@Mac.lan>
2025-09-16 03:38:09 +0400

    add the config service

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,10 +2,10 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   type: 'mysql',
-  host: process.env.DB_HOST || '127.0.0.1',
-  port: process.env.DB_PORT || 3306,
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'hrm',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   synchronize: process.env.DB_SYNC === 'true',
 }));
