5a66d41a9268f74638bb29eb29e09c68819acdeb
Dafallah Ibrahim <dodokaka10@gmail.com>
2025-09-17 15:54:53 +0400

    feat: enhance GetProfileDto with ApiProperty decorator for Swagger documentation

--- src/auth/dto/get-profile.dto.ts
+++ src/auth/dto/get-profile.dto.ts
@@ -1,6 +1,8 @@
 import { IsInt } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GetProfileDto {
+  @ApiProperty({ example: 1 })
   @IsInt()
   id: number;
 }
